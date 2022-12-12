6dca81ba868be41e5e138b4162f43678591e869d
DrC0okie <timothee.vanhove@heig-vd.ch>
2022-12-12 09:47:55 +0100

    Tryed hard to make this node program works....

--- Express server/src/index.js
+++ Express server/src/index.js
@@ -1,1 +1,25 @@
 //Push to git
+var express = require('express');
+var app = express();
+
+const Chuck  = require('chucknorris-io'),
+      client = new Chuck();
+
+
+app.get('/', (req, res)=>{
+	console.log("Received request")
+	res.send(generateJoke());
+});
+
+function generateJoke(){
+	// Retrieve a random chuck joke
+	return client.getRandomJoke().then(function (response) {
+    // do stuff here
+	}).catch(function (err) {
+    // handle error
+	});
+};
+
+app.listen(3000, () => {
+  console.log('Accepting HTTP requests on port 3000.')
+})
