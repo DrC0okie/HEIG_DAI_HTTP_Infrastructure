1bff73d15d1e83075684bbd84715b6f75d0b7552
Jarod <jarod.streckeisen@heig-vd.ch>
2023-01-03 15:58:51 +0100

    Button color change + Working start / stop container

--- YADMGUI/routes/containers.js
+++ YADMGUI/routes/containers.js
@@ -4,12 +4,42 @@ const router = express.Router();
 const docker = new Docker();
 
 
-/* GET users listing. */
+/* GET all containers. */
 router.get('/', function(req, res, next) {
   docker.listContainers({all:true}).then(containers=>{
-    console.log(containers);
     res.render('containers',{containers});
   })
 });
+router.put('/run',function (req,res,next){
+  const container = docker.getContainer(req.body.id);
+  container.start().then(info =>{
+    res.status(200).json({msg: "Container started"});
+  }).catch(err=>{
+    res.status(err.statusCode).json({err:err.json.message});
+  });
+})
+router.put('/stop',function (req,res,next){
+  const container = docker.getContainer(req.body.id);
+  container.stop().then(info =>{
+    res.status(200).json({msg: "Container started"});
+  }).catch(err=>{
+    res.status(err.statusCode).json({err:err.json.message});
+  });
+})
+
+router.put('/runAll',function (req,res,next){
+  docker.listContainers(function (err, containers) {
+    containers.forEach(function (containerInfo) {
+      docker.getContainer(containerInfo.Id).start(cb);
+    });
+  });
+})
+router.put('/stopAll',function (req,res,next){
+  docker.listContainers(function (err, containers) {
+    containers.forEach(function (containerInfo) {
+      docker.getContainer(containerInfo.Id).stop(cb);
+    });
+  });
+})
 
 module.exports = router;
