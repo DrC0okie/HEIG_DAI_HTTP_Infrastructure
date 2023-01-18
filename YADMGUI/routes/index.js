5ca5518c25f6a715ba1f575517b25ca390679aa1
Jarod <jarod.streckeisen@heig-vd.ch>
2023-01-18 14:11:23 +0100

    Clean YADMGUI

--- YADMGUI/routes/index.js
+++ YADMGUI/routes/index.js
@@ -5,7 +5,7 @@ const docker = new Docker();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
